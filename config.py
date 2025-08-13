d3691966f864e63f2898fcfb00b454b0c27f876c
gcamaral2105 <gabrieldecarvalho2105@gmail.com>
2025-08-13 17:03:04 -0300

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,11 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MIGRATE_RENDER_AS_BATCH = True
 
 class DevelopmentConfig(Config):
     DEBUG = True
 
 class ProductionConfig(Config):
-
     DEBUG = False
+
