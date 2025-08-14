b1dfeb88b04caba410917f110c035d8edecd9a79
gcamaral2105 <gabrieldecarvalho2105@gmail.com>
2025-08-14 17:14:50 -0300

    Update __init__.py

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -16,10 +16,22 @@ CORS(api_v1, origins='*')
 
 # Import resources to register routes
 from .resources import production_api, vessel_api, partner_api, auth_api
+from .resources.lineup_api import lineup_bp
+from .resources.shuttle_api import shuttle_bp
+from .resources.vld_api import vld_bp
+from .resources.capesize_api import capesize_bp
+from .resources.scheduling_api import scheduling_bp
 
 # Register resource blueprints
 api_v1.register_blueprint(production_api.bp)
 api_v1.register_blueprint(vessel_api.bp)
 api_v1.register_blueprint(partner_api.bp)
 api_v1.register_blueprint(auth_api.bp)
+api_v1.register_blueprint(lineup_bp)
+api_v1.register_blueprint(shuttle_bp)
+api_v1.register_blueprint(vld_bp)
+api_v1.register_blueprint(capesize_bp)
+api_v1.register_blueprint(scheduling_bp)
+
+
 
