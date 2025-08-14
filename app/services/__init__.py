79df70f04d75c0c9e877c1852d19b192f109dd37
gcamaral2105 <gabrieldecarvalho2105@gmail.com>
2025-08-14 16:57:34 -0300

    Update __init__.py

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -8,10 +8,20 @@ Centralized imports for all service classes in the ERP Bauxita system.
 from .production_service import ProductionService
 from .vessel_service import VesselService
 from .partner_service import PartnerService
+from .lineup_service import LineupService
+from .shuttle_service import ShuttleService
+from .vld_service import VLDService
+from .capesize_service import CapesizeService
+from .scheduling_service import SchedulingService
 
 __all__ = [
     'ProductionService',
     'VesselService',
-    'PartnerService'
+    'PartnerService',
+    'LineupService',
+    'ShuttleService',
+    'VLDService',
+    'CapesizeService',
+    'SchedulingService'
 ]
 
