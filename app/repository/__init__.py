d97c89f570c36b333e8e0207997c3da6ee936d1c
gcamaral2105 <gabrieldecarvalho2105@gmail.com>
2025-08-14 17:07:15 -0300

    Update __init__.py

--- app/repository/__init__.py
+++ app/repository/__init__.py
@@ -8,9 +8,19 @@ Centralized imports for all repository classes in the ERP Bauxita system.
 from .production_repository import ProductionRepository
 from .vessel_repository import VesselRepository
 from .partner_repository import PartnerRepository
+from .lineup_repository import LineupRepository
+from .shuttle_repository import ShuttleRepository, ShuttleOperationRepository
+from .capesize_repository import CapesizeRepository
+from .vld_repository import VLDRepository
 
 __all__ = [
     'ProductionRepository',
     'VesselRepository', 
-    'PartnerRepository'
+    'PartnerRepository',
+    'LineupRepository',
+    'ShuttleRepository',
+    'ShuttleOperationRepository',
+    'CapesizeRepository',
+    'VLDRepository'
 ]
+
